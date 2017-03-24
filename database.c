b5f2a3cfbafa8949ad299b7d08a9fd1cef9d83b2
vinilopes03 <vinilopes03@gmail.com>
2017-03-24 18:41:23 -0300

    Add commentaries

--- database.c
+++ database.c
@@ -9,7 +9,7 @@
 
 
 /* Basic database struct */
-typedef struct 
+typedef struct
 {
     FILE *file;
     size_t entry_size;
@@ -18,6 +18,9 @@ typedef struct
 
 /*
     Instantiate a database
+    @param array of char
+    @param size_t variable;
+    @return pointer for database_t
 */
 database_t *instantiate_database(char[] data_file_path, size_t entry_size)
 {
@@ -31,39 +34,65 @@ database_t *instantiate_database(char[] data_file_path, size_t entry_size)
     database->data_file_path = data_file_path;
 }
 
-/* Put a entry in database */
+/* Put a entry in database
+    @param pointer for database_t
+    @param a void pointer
+    @return void;
+    */
 void create_entry(database_t *database, void *entry){
     fseek(database->file, 0L, SEEK_END);
     fwrite(entry, database->entry_size, 1, database->file);
 }
-
+/*
+    Seek for an entry in database
+    @param database_t pointer
+    @param void pointer;
+    @param a boolean pointer funtion (@param void pointer, @param void pointer);
+    @return 1 if TRUE, 0 if FALSE
+    */
 bool seek_entry(database_t *database, void *entry_target, bool (*entry_comparator)(void*, void*)){
     rewind(database->file);
-    void *entry = malloc(database->entry_size);    
+    void *entry = malloc(database->entry_size);
     bool found = false;
     while(!found && fread(entry, database->entry_size, 1, database->file) == database->entry_size)
-        found = (*entry_comparator)(entry_target, entry) 
-    
+        found = (*entry_comparator)(entry_target, entry)
+
     if(found) fseek(database->file, fteel(database->file) - database->entry_size);
     free(entry);
     return found;
 }
 
-/* Read a entry from database */
+/* Read a entry from database
+    @param database_t pointer
+    @param void pointer
+    @param a boolean function (@param void pointer, @param void pointer)
+    @return void
+    */
 void *read_entry(database_t *database, void *entry_target, bool (*entry_comparator)(void*, void*)){
-    if(!seek_entry(database, entry_target, entry_comparator)) return NULL //Not found   
+    if(!seek_entry(database, entry_target, entry_comparator)) return NULL //Not found
     void *entry = malloc(database->entry_size);
     fread(entry, database->entry_size, 1, database->file);
     return entry;
 }
 
-/* Update a entry from database */
+/* Update a entry from database
+    @param database_t pointer
+    @param void pointer
+    @param @param a boolean function (@param void pointer, @param void pointer)
+    @param void pointer
+    @return void
+    */
 void update_entry(database_t *database, void *entry_target, bool (*entry_comparator)(void*, void*), void *updated_entry){
-    if(!seek_entry(database, entry_target, entry_comparator)) return NULL //Not found   
+    if(!seek_entry(database, entry_target, entry_comparator)) return NULL //Not found
     fwrite(updated_entry, database->entry_size, 1, database->file);
 }
 
-/* Read a entry from database */
+/* Read a entry from database
+    @param database_t pointer
+    @param void pointer
+    @@param a boolean function (@param void pointer, @param void pointer)
+    @return void
+    */
 void delete_entry(database_t *database, void *entry_target, bool (*entry_comparator)(void*, void*)){
     FILE tmp_data_file = fopen(database->data_file_path ".tmp", "w");
     rewind(database->file);
@@ -74,11 +103,11 @@ void delete_entry(database_t *database, void *entry_target, bool (*entry_compara
         else fwrite(entry, database->entry_size, 1, tmp_data_file);
     }
     free(entry);
-    
+
     if(found) {
         remove(database->data_file_path);
-        rename(database->data_file_path ".tmp", database->data_file_path);    
+        rename(database->data_file_path ".tmp", database->data_file_path);
     }
-    
+
     return;
 }
